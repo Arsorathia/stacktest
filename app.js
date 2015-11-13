1e0ae3230ec31b03c61367e94f588904948c4145
Arif Sorathia <arifsorathia@Arifs-MBP.att.net>
2015-11-13 12:45:33 -0800

    Updated 3

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ http.createServer(function(req, res) {
     '\n<meta charset="utf-8">\n<title>Test web page on node.js</title>\n' + 
     '<style type="text/css">* {font-family:arial, sans-serif;}</style>\n' + 
     '\n\n<h1>Euro 2012 teams</h1>\n' + 
-    '<div id="content"><p>The Arif Is Cool teams in Group D for Euro 2012 are:</p><ul><li>England</li><li>France</li><li>Sweden</li><li>Ukraine</li></ul></div>' + 
+    '<div id="content"><p>The Arif Loves Stackahoy  Is Cool teams in Group D for Euro 2012 are:</p><ul><li>England</li><li>France</li><li>Sweden</li><li>Ukraine</li></ul></div>' + 
     '\n\n');
   res.end();
 }).listen(8888);
